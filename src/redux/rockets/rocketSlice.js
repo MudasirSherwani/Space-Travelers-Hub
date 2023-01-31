e239a21208b7acadf85fdb62982dc4dc2da83e13
admirerbrown <admirerbrown@gmail.com>
2023-01-31 11:57:16 +0000

    build phase 1 of rockets page and functionality

---
+++ src/redux/rockets/rocketSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// action type ...
+const FETCHROCKETS = 'spaceTravelsHub/FETCHROCKETS';
+
+// fetch rockets...
+// const GetRockets = async () => {
+//   const url = 'https://api.spacexdata.com/v4/rockets';
+//   const res = await axios.get(url);
+
+// };
+
+const GetRockets = createAsyncThunk(FETCHROCKETS, async () => {
+  const url = 'https://api.spacexdata.com/v4/rockets';
+  const res = await axios.get(url);
+  const rocketList = res.data;
+  return rocketList;
+});
+
+// reducer...
+const rocketReducer = createSlice({
+  name: 'rocketApi',
+  initialState: {
+    data: [],
+    isFulfilled: false,
+  },
+
+  reducers: {},
+  extraReducers: {
+    [GetRockets.fulfilled]: (state, action) => {
+      state.isFulfilled = true;
+      state.data = action.payload;
+    },
+  },
+});
+
+export default rocketReducer;
+export { GetRockets };
