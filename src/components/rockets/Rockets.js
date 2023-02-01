5c6ba38a56e767dc7d778c5848b2b88197dc1d2d
admirerbrown <admirerbrown@gmail.com>
2023-02-01 12:18:36 +0000

    able to add reserve to rocket

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,19 +1,35 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bookRocket } from '../../redux/rockets/rocketSlice';
 
 const RocketsPage = () => {
+  const dispatch = useDispatch();
   const rocketList = useSelector((state) => state.rockets.data);
+
+  const onClickHandler = (id) => {
+    dispatch(bookRocket({ id, rocketList }));
+  };
 
   return (
     <div className="container">
       {rocketList.map((rocket) => (
         <div key={rocket.id} className="rocket-card">
           <div className="">
-            <img src={rocket.flickr_images[0]} alt="rocket-launch" className="rocket-img" />
+            <img
+              src={rocket.flickr_images[0]}
+              alt="rocket-launch"
+              className="rocket-img"
+            />
           </div>
           <div className="rocket-details">
             <h3 className="title ">{rocket.name}</h3>
             <p className="description">{rocket.description}</p>
-            <button type="button" className="reserve-btn">Reserve Rocket</button>
+            <button
+              type="button"
+              className="reserve-btn"
+              onClick={() => onClickHandler(rocket.id)}
+            >
+              Reserve Rocket
+            </button>
           </div>
         </div>
       ))}
--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -3,12 +3,18 @@ import axios from 'axios';
 
 // action type ...
 const FETCHROCKETS = 'spaceTravelsHub/FETCHROCKETS';
+const BOOKROCKET = 'spaceTravelsHub/BOOKROCKET';
 
 const GetRockets = createAsyncThunk(FETCHROCKETS, async () => {
   const url = 'https://api.spacexdata.com/v4/rockets';
   const res = await axios.get(url);
   const rocketList = res.data;
   return rocketList;
+});
+
+export const bookRocket = (payload) => ({
+  type: BOOKROCKET,
+  payload,
 });
 
 // reducer...
@@ -20,11 +26,20 @@ const rocketReducer = createSlice({
   },
 
   reducers: {},
-  extraReducers: {
-    [GetRockets.fulfilled]: (state, action) => {
+  extraReducers: (builder) => {
+    builder.addCase(GetRockets.fulfilled, (state, action) => {
       state.isFulfilled = true;
       state.data = action.payload;
-    },
+    });
+
+    builder.addCase(BOOKROCKET, (state, action) => {
+      const newState = state.data.map((rocket) => {
+        if (rocket.id !== action.payload.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+      state.data = newState;
+      // console.log(JSON.parse(JSON.stringify(state.data)));
+    });
   },
 });
 
